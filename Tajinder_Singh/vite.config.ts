2f80168b2ac7a7c7ba854ef552a56c277dea6f29
Tajinder Singh <singhtajinder9888@gmail.com>
2025-10-04 22:02:51 +0530

    updated viteconfig

--- Tajinder_Singh/vite.config.ts
+++ Tajinder_Singh/vite.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
   server: {
     host: true,               
     strictPort: true,         
-    allowedHosts: ['tajinder.xyz'], 
+    allowedHosts: ['tajinder.xyz','www.tajinder.xyz'], 
   }
 })
