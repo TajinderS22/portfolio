b22316df2bcb73f20d059c08e8d30fc29907b15a
Tajinder Singh <singhtajinder9888@gmail.com>
2025-10-04 21:24:57 +0530

    updated viteconfig 001

--- Tajinder_Singh/vite.config.ts
+++ Tajinder_Singh/vite.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
   server: {
     host: true,               
     strictPort: true,         
-    // allowedHosts: ['tajinder.xyz'], // alternative way to whitelist domain
+    allowedHosts: ['tajinder.xyz'], 
   }
 })
